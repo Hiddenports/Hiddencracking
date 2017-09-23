2200e328f6b02a0e4dfd6a9bdf7757531a453354
TUX-PC\Dylan <kernelpanics68@gmail.com>
2017-09-23 17:24:31 +0200

    Made wordlist.c ready for use

--- wordlist.c
+++ wordlist.c
@@ -5,45 +5,41 @@
 int main(int argc, char *argv[]) {
 	char c, buffer[100];
 	int len, id, i;
-	if(argc != 3) {
-		printf("[ERROR] Usage:  %s <string> <length>\n", argv[0]);
+	if(argc != 2) {
+		printf("[ERROR] Usage: %s <length>\n", argv[0]);
 		exit(1);
 	}
-	if((len = atoi(argv[2])) == NULL) {
+	if((len = atoi(argv[1])) == NULL) {
 		printf("Second parameter is invalid...\n");
 		exit(2);
 	}
+	if(len >= 100) {
+		printf("Max-Length: 99\n");
+		exit(1);
+	}
 	printf("Length: %d\n", len);
-	id = len;
+	id = (len - 1);
 	printf("Starting test for %s\n", argv[1]);
-	memset(buffer, 49, sizeof(buffer));
+	memset(&buffer, '\0', sizeof(buffer));
+	memset(&buffer, 49, len);
 	while(id >= 0) {
-		if((id != (len - 1)) && (buffer[id] != 122)) {
+		if((id != (len - 1)) && (buffer[id] != 'z')) {
 			c = buffer[id];
-			memset(buffer[id], c++, 1);
+			memset(&buffer[id], c + 1, 1);
 			id = (len - 1);
 		}
-		printf("Debug: 1");
-		else if((id != (len - 1)) && (buffer[id] == 122)) {
-			memset(buffer[id], 49, 1);
+		else if((id != (len - 1)) && (buffer[id] == 'z')) {
+			memset(&buffer[id], 49, 1);
 			id--;
 		}
-		else if((id == (len - 1)) && (buffer[id] == 122))
+		else if((id == (len - 1)) && (buffer[id] == 'z'))
 			id--;
 		else if(id == (len - 1)) {
-			for(buffer[id] = 49; buffer[id] <= 122; buffer[id]++) {
+			for(buffer[id] = 49; buffer[id] <= 'z'; buffer[id]++) {
 				printf("%s\n", buffer);
-				if(strcmp(buffer, argv[1]) == 0) {
-					printf("Found code: %s\n", buffer);
-					return 0;
-				}
 			}
-		}
-		if(strcmp(buffer, argv[1]) == 0) {
-			printf("Found code: %s\n", buffer);
-			return 0;
+			id--;
 		}
 	}
-	printf("Code not found.\n");
-	return 1;
+	return 0;
 }
